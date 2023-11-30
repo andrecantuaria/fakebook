96de59c721021c551bbaa9c52152e1e6b6b0172e
Andre Cantuaria <andrecantuaria@gmail.com>
2023-11-29 20:42:47 -0600

    Finish Modal and get info

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,50 +1,54 @@
 'use strict';
 
-// utility functions utility-functions.js
+// Utility functions utility-functions.js
 function onEvent(event, selector, callback) {
     return selector.addEventListener(event, callback);
-  }
-  
-  function getElement(selector, parent = document) {
-    return parent.getElementById(selector);
-  }
-  
-  function select(selector, parent = document) {
-    return parent.querySelector(selector);
-  }
-  
-  function selectAll(selector, parent = document) {
-    return [...parent.querySelectorAll(selector)];
-  }
+}
 
+function getElement(selector, parent = document) {
+    return parent.getElementById(selector);
+}
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+function selectAll(selector, parent = document) {
+    return [...parent.querySelectorAll(selector)];
+}
 
 // Classes user.js
 
-// parent
+// Parent
 class User {
     #id;
     #name;
-    #userName; 
+    #userName;
     #email;
 
-    constructor(id, name, userName, email) { 
+    constructor(id, name, userName, email) {
         this.#id = id;
         this.#name = name;
-        this.#userName = userName; 
+        this.#userName = userName;
         this.#email = email;
     }
 
     get id() { return this.#id; }
     get name() { return this.#name; }
-    get userName() { return this.#userName; } 
+    get userName() { return this.#userName; }
     get email() { return this.#email; }
 
     getInfo() {
-        return `${this.#id} ${this.#name} ${this.#userName} ${this.#email}`;
+        return {
+            id: this.#id,
+            name: this.#name,
+            userName: this.#userName,
+            email: this.#email
+        };
     }
 }
 
-// child
+// Child
 class Subscriber extends User {
     #pages;
     #groups;
@@ -62,66 +66,64 @@ class Subscriber extends User {
     get canMonetize() { return this.#canMonetize; }
 
     getInfo() {
-        return `${this.#pages} ${this.#groups} ${this.#canMonetize}`;
+        return {
+            ...super.getInfo(),
+            pages: this.#pages,
+            groups: this.#groups,
+            canMonetize: this.#canMonetize
+        };
     }
 }
 
 const user = new Subscriber(
-    '1',
-    'Joe Doe',
-    'jdoe',
-    'jdoe@email.com',
-    ['Page1', 'Page 2', 'Page 3'], 
-    ['Group 1', 'Group 2', 'Group 3'], 
+    '1001',
+    'Andre Cantuaria',
+    'aplusc',
+    'acantuaria@email.com',
+    ['Tech Insights', 'Travel Journals', 'Fitness Focus', 'Foodie Delights'],
+    ['Tech Trends & Innovations', 'Adventure Explorers', 'Fitness Enthusiasts', 'Artistic Expressions Guild'],
     true
-  );
+);
 
 // Main Code index.js
 
-console.log(user.id)
-
-const userInfo = select('.user-info');
-userInfo.innerHTML = user.getInfo();
-
-
 // DOM Elements
 const profile = select('.profile');
-const input = select('.input');
-const uploadImgBtn = select('.upload-img-btn');
-const uploadInfo = select('.upload-info');
-const postBtn = select('post-btn');
-const username = select('username');
-const date = select('date');
-const postTextWithImg = select('.post-text-with-img');
-const postTextWithoutImg = select('.post-text-without-img');
-const postImg = select('.post-img');
-
+const fullName = select('.name');
+const userName = select('.username');
+const pages = select('.pages');
+const groups = select('.groups');
 
 // Modal
 
 onEvent('DOMContentLoaded', document, function () {
     const profileButton = select('.profile');
     onEvent('click', profileButton, openModal);
-  });
-  
-  // open the modal
-  function openModal() {
+});
+
+// Open the modal
+function openModal() {
     const modal = getElement('myModal');
     modal.style.display = 'flex';
-  }
-  
-  // close the modal
-  function closeModal() {
+
+// Display user info
+    const userInfo = user.getInfo();
+    fullName.innerHTML = `<p><strong>Name:</strong> ${userInfo.name}</p>`;
+    userName.innerHTML = `<p><strong>Username:</strong> ${userInfo.userName}</p>`;
+    pages.innerHTML = `<p><strong>Pages:</strong> ${userInfo.pages.join(', ')}</p>`;
+    groups.innerHTML = `<p><strong>Groups:</strong>${userInfo.groups.join(', ')}`;
+}
+
+// Close the modal
+function closeModal() {
     const modal = getElement('myModal');
     modal.style.display = 'none';
-  }
-  
-  // Close the modal if the overlay is clicked
-  onEvent('click', window, function (event) {
+}
+
+// Close the modal if the overlay is clicked
+onEvent('click', window, function (event) {
     const modal = getElement('myModal');
     if (event.target === modal) {
-      modal.style.display = 'none';
+        modal.style.display = 'none';
     }
-  });
-
-
+});
